9ed707c6382d28b0dbd0f3f4a4d74b4cf4b8a96c
kokistr <125700374+kokistr@users.noreply.github.com>
2024-12-14 05:50:22 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 DB_CONFIG = {
-    'host': os.getenv('DB_HOST', 'localhost'),
-    'user': os.getenv('DB_USER', 'root'),
-    'password': os.getenv('DB_PASSWORD', 'new_password'),
+    'host': os.getenv('DB_HOST', 'tech0-gen-8-step3-rdb-14.mysql.database.azure.com'),
+    'user': os.getenv('DB_USER', 'tech0gen8student'),
+    'password': os.getenv('DB_PASSWORD', '5iTbVNuqQu8z14'),
     'database': os.getenv('DB_NAME', 'recipes_table')
 }
